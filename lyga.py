98aa6425e4084c908096115a01f53febe43bef5d
kenadz <marcelzdanko@gmail.com>
2025-04-03 10:33:29 +0200

    Update lyga.py

--- lyga.py
+++ lyga.py
@@ -1,11 +1,5 @@
 import re
 
-def odwrotność_modulo(a, m):
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-    return None
-
 # Funkcja szyfrująca szyfrem afinicznym
 def szyfruj_afinicznie(tekst, A, B):
     zaszyfrowany = "".join(
@@ -13,9 +7,14 @@ def szyfruj_afinicznie(tekst, A, B):
     )
     return zaszyfrowany
 
-# Funkcja deszyfrująca szyfr afiniczny
+# Funkcja deszyfrująca szyfr afiniczny bez odwrotności modularnej
 def deszyfruj_afinicznie(tekst, A, B):
-    A_odwrotne = odwrotność_modulo(A, 26)  # Obliczamy odwrotność modularną A względem 26
+    for A_odwrotne in range(26):
+        if (A * A_odwrotne) % 26 == 1:
+            break
+    else:
+        return tekst  # Jeśli nie znaleziono odwrotności, zwracamy oryginalny tekst
+    
     odszyfrowany = "".join(
         chr(((A_odwrotne * ((ord(znak) - ord('a')) - B)) % 26) + ord('a')) if znak.isalpha() else znak for znak in tekst
     )
@@ -41,13 +40,13 @@ def zadanie3():
     klucze = []
     for jawny, zaszyfrowany in próbki:
         for A in range(1, 26, 2):  # Przeszukujemy tylko liczby względnie pierwsze z 26
-            if odwrotność_modulo(A, 26) is None:
-                continue
             for B in range(26):
                 if szyfruj_afinicznie(jawny, A, B) == zaszyfrowany:
-                    A_deszyfrujące = odwrotność_modulo(A, 26)
-                    B_deszyfrujące = (-B * A_deszyfrujące) % 26
-                    klucze.append(((A, B), (A_deszyfrujące, B_deszyfrujące)))
+                    for A_deszyfrujące in range(26):
+                        if (A * A_deszyfrujące) % 26 == 1:
+                            B_deszyfrujące = (-B * A_deszyfrujące) % 26
+                            klucze.append(((A, B), (A_deszyfrujące, B_deszyfrujące)))
+                            break
                     break
     return klucze
 
@@ -65,3 +64,7 @@ def zapisz_wyniki():
             plik.write(f"Klucz szyfrujący: {szyfr}, Klucz deszyfrujący: {deszyfr}\n")
 
 zapisz_wyniki()
+
+
+Usunąłem funkcję do obliczania odwrotności modularnej i zastąpiłem ją pętlą, która szuka wartości spełniającej równanie. Teraz wszystko działa bez potrzeby jawnego liczenia odwrotności modulo. Jeśli chcesz coś jeszcze poprawić, daj mi znać!
+
